tentative fix for nan gradients

linguamechanica/agent.py
@@ -333,8 +333,18 @@ class IKAgent:
                 self.training_state.t,
             )
 
-    def actor_geodesic_optimization(self, state, epsilon=1e-3):
+    def is_corrupted(self, tensor):
+        return torch.logical_or(torch.isinf(tensor), torch.isnan(tensor))
+
+    def corrupted_rows(self, tensor):
+        corr = self.is_corrupted(tensor).sum(1) > 0        
+        return tensor[corr, :]
+
+
+    def actor_geodesic_optimization(self, state):
         thetas, target_pose = Environment.thetas_target_pose_from_state(state)
+        thetas.requires_grad=True
+        target_pose.requires_grad=True
         initial_reward = Environment.compute_reward(
             self.open_chain, thetas, target_pose, self.training_state.weights
         ).mean()
@@ -357,11 +367,11 @@ class IKAgent:
                 final_reward,
                 self.training_state.t,
             )
-        # loss = -final_reward
-        reward_times_worse = (
-            (final_reward - initial_reward) / (initial_reward + epsilon)
+        loss = (
+            (final_reward - initial_reward.data) / (initial_reward.data + 1e-3)
         ).mean()
-        loss = reward_times_worse
+        #self.actor.retain_grad()
+        loss.retain_grad()
         if self.summary is not None:
             self.summary.add_scalar(
                 "Train / Actor Reward Times Worse Loss",
@@ -369,7 +379,31 @@ class IKAgent:
                 self.training_state.t,
             )
         self.actor_geodesic_optimizer.zero_grad()
+        thetas.retain_grad()
+        target_pose.retain_grad()
         loss.backward()
+        '''
+
+        '''
+        for idx, param in enumerate(self.actor.parameters()):            
+            if param.grad is not None:
+                if self.is_corrupted(param).sum() > 0 or self.is_corrupted(param.grad).sum() > 0 or self.is_corrupted(loss).sum() > 0:
+                    print("------------- Actor Grad -------------")
+                    torch.set_printoptions(precision=20)
+                    print(self.is_corrupted(param).sum() > 0, self.is_corrupted(param.grad).sum() >0 ,  self.is_corrupted(loss).sum() > 0)
+                    print(f"Time Step {self.training_state.t}")
+                    print(f"Current Gradient: {idx}")
+                    print("loss", loss)
+                    print("self.corrupted_rows(thetas.grad)", self.corrupted_rows(thetas.grad))
+                    print("self.corrupted_rows(target_pose.grad)", self.corrupted_rows(target_pose.grad))                    
+                    print("param", param)
+                    print("param.grad.shape", param.grad.shape)
+                    print("param.grad", self.corrupted_rows(param.grad))
+                    print("thetas", thetas)
+                    print("target_pose", target_pose)
+                    rows_with_corrupted_values = self.is_corrupted(target_pose.grad).sum(1) > 0
+                    print("param.grad with rows_with_corrupted_values", param.grad[rows_with_corrupted_values, :])
+                    print("--------------------------------------")
         torch.nn.utils.clip_grad_norm_(
             self.actor.parameters(), self.training_state.gradient_clip_actor_geodesic()
         )
@@ -423,7 +457,39 @@ class IKAgent:
         self.total_it += 1
         state, action, reward, next_state, done = self.sample_from_buffer()
         self.actor_geodesic_optimization(state)
-        self.critic_update(state, reward, next_state, done)
-        self.delayed_actor_update(state)
-        self.actor_entropy_update(state)
+        #self.critic_update(state, reward, next_state, done)
+        #self.delayed_actor_update(state)
+        #self.actor_entropy_update(state)
         self.update_target_models()
+
+
+if __name__ == "__main__":
+    from linguamechanica.kinematics import UrdfRobotLibrary
+    from linguamechanica.se3 import ImplicitDualQuaternion
+    urdf_robot = UrdfRobotLibrary.dobot_cr5()
+    se3 = ImplicitDualQuaternion()    
+    open_chain = urdf_robot.extract_open_chains(se3, 0.1)[-1].cuda()
+    target_pose = torch.tensor([[ 0.31304278969764709473, -0.00631798803806304932,
+        1.22056627273559570312, -1.25425827503204345703,
+        0.11632148176431655884,  0.80687266588211059570]], device='cuda:0',
+        requires_grad=True)
+    thetas = torch.tensor([[-3.51662302017211914062,  0.27089971303939819336,
+        3.34850096702575683594, -2.87017297744750976562,
+        0.16795067489147186279,  0.00000000000000000000000000001]], device='cuda:0',
+        requires_grad=True)
+    thetas.retain_grad()
+    target_pose.retain_grad()
+    #current_pose = 
+    loss = Environment.compute_reward(
+            open_chain, thetas, target_pose, torch.ones(6).cuda()).mean()
+    print(loss)
+    #loss = se3.exp(target_pose).mean()
+    print(target_pose.grad)
+    #open_chain = urdf_robot.extract_open_chains(se3, 0.1)[-2].cuda()
+    #loss = open_chain.forward_kinematics(thetas[:, :5]).mean()
+    #loss.backward()
+    #print(thetas.grad)
+    #open_chain = urdf_robot.extract_open_chains(se3, 0.1)[-1].cuda()
+    #loss = open_chain.forward_kinematics(thetas[:, :]).mean()
+    #loss.backward()
+    #print(thetas.grad)

linguamechanica/se3.py
@@ -105,7 +105,7 @@ class ImplicitDualQuaternion(SE3):
     Like Dual Quaternions, `h = hv + hw`, where `hv=(x, y, z)` and `hw = w`
     """
 
-    def __init__(self, epsilon=1e-3):
+    def __init__(self, epsilon=1e-1):
         self.epsilon = epsilon
 
     def act_vector(self, idq, vector):
@@ -145,26 +145,29 @@ class ImplicitDualQuaternion(SE3):
         omega = torch.norm(w, p=2, dim=1, keepdim=True)
         cos = omega.cos()
         sin = omega.sin()
-        mu_r = sin / omega
         # TODO: it might be possible to only compute a subset of `omega_square`
         # and `omega_quartic`
         omega_square = omega * omega
         omega_quartic = omega_square * omega_square
+
+        mu_r = torch.zeros_like(omega)
         mu_r_singularity = omega.abs() < self.epsilon
-        if mu_r_singularity[mu_r_singularity == True].shape[0] > 0:
-            mu_r[mu_r_singularity] = (
-                1.0
-                - (omega_square[mu_r_singularity] / 6.0)
-                + (omega_quartic[mu_r_singularity] / 120.0)
-            )
-        mu_d = (2.0 - (cos * 2.0 * mu_r)) / omega_square
-        mu_d_singularity = omega_square < self.epsilon
-        if mu_d_singularity[mu_d_singularity == True].shape[0] > 0:
-            mu_d[mu_d_singularity] = (
-                (4.0 / 3.0)
-                - ((4.0 * omega_square[mu_d_singularity]) / 15.0)
-                + ((8.0 * omega_quartic[mu_d_singularity]) / 315.0)
-            )
+        mu_r[~mu_r_singularity] = sin[~mu_r_singularity] / omega[~mu_r_singularity]
+        mu_r[mu_r_singularity] = (
+            1.0
+            - (omega_square[mu_r_singularity] / 6.0)
+            + (omega_quartic[mu_r_singularity] / 120.0)
+        )
+
+        mu_d = torch.zeros_like(omega)
+        mu_d_singularity = omega_square.abs() < self.epsilon
+        mu_d[~mu_d_singularity] = (2.0 - (cos[~mu_d_singularity] * 2.0 * mu_r[~mu_d_singularity])) / omega_square[~mu_d_singularity]        
+        mu_d[mu_d_singularity] = (
+            (4.0 / 3.0)
+            - ((4.0 * omega_square[mu_d_singularity]) / 15.0)
+            + ((8.0 * omega_quartic[mu_d_singularity]) / 315.0)
+        )
+
         # TODO: this inner product should be computed w.o. einsum
         sigma = torch.einsum("bi,bi->b", coord_v, w).unsqueeze(1)
         h = torch.cat([mu_r * w, cos], 1)
@@ -172,6 +175,66 @@ class ImplicitDualQuaternion(SE3):
         cross = torch.cross(hv, coord_v)
         v = ((2.0 * mu_r) * cross) + (cos * 2.0 * mu_r * coord_v) + (mu_d * sigma * w)
         return torch.cat([h, v], 1)
+
+    def grad_safe_divisor(self, data, singularity_mask):
+        sign = torch.sign(data[singularity_mask])
+        sign[sign == 0.0] = 1.0
+        return data[singularity_mask, :].data  # + (1e-8 * sign)
+
+    def atan2(self, sin, cos):
+        """
+        This function computes `atan2` keeping safe gradients assuming
+        the arguments are `sin` and `cos`.
+        Example of problem with pytorch and small denominator with Nan Gradients:
+            https://discuss.pytorch.org/t/how-to-avoid-nan-output-from-atan2-during-backward-pass/176890
+        For implementation details see https://en.wikipedia.org/wiki/Atan2
+
+        Note that this atan2 implementation only works for the log as
+        it assumes that if sin is close to 0, sin will be close to +1 or -1
+        """
+        result = torch.ones_like(sin)
+        x_is_near_zero = cos.abs() <= self.epsilon
+        x_is_positive = cos > self.epsilon
+        x_is_negative = cos < -self.epsilon
+        # y_is_near_zero = sin.abs() <= self.epsilon
+        y_is_positive = sin >= 0.0
+        y_is_negative = sin < 0.0
+        result[x_is_positive] = torch.arctan(sin[x_is_positive] / cos[x_is_positive])
+
+        # if cos is close to 0, sin will be close to +1 or -1
+        if x_is_near_zero.sum() > 0:
+            x_cube = cos[x_is_near_zero] ** 3
+            x_five = x_cube * (cos[x_is_near_zero] ** 2)
+            y_cube = sin[x_is_near_zero] ** 3
+            y_five = y_cube * (sin[x_is_near_zero] ** 2)
+            result[x_is_near_zero] = (
+                (torch.pi / 2.0)
+                - (cos[x_is_near_zero] / sin[x_is_near_zero])
+                + (x_cube / (3.0 * y_cube))
+                - (x_five / (y_five * 5.0))
+            )
+
+        x_is_negative_and_y_is_negative = torch.logical_and(
+            x_is_negative, y_is_negative
+        )
+        result[x_is_negative_and_y_is_negative] = (
+            torch.arctan(
+                sin[x_is_negative_and_y_is_negative]
+                / cos[x_is_negative_and_y_is_negative]
+            )
+            - torch.pi
+        )
+        x_is_negative_and_y_is_positive = torch.logical_and(
+            x_is_negative, y_is_positive
+        )
+        result[x_is_negative_and_y_is_positive] = (
+            torch.arctan(
+                sin[x_is_negative_and_y_is_positive]
+                / cos[x_is_negative_and_y_is_positive]
+            )
+            + torch.pi
+        )
+        return result
 
     def log(self, implicit_dual_quaternion_batch):
         # zero_div_eps = 1e-12
@@ -181,49 +244,37 @@ class ImplicitDualQuaternion(SE3):
         s = torch.norm(hv, p=2, dim=1, keepdim=True)
         c = self.extract_hw(h)
 
-        # Do not forward close-to-zero denominator gradients
-        theta_singularity = c.abs().squeeze() < self.epsilon
-        theta = torch.zeros_like(c)
-        if (~theta_singularity).sum() > 0:
-            theta[~theta_singularity, :] = torch.atan2(
-                s[~theta_singularity, :], c[~theta_singularity, :]
-            )
-        if theta_singularity.sum() > 0:
-            theta[theta_singularity, :] = torch.atan2(
-                s[theta_singularity, :], c.data[theta_singularity, :]
-            )
-
+        theta = self.atan2(s, c)
         theta_square = theta * theta
         theta_fourth = theta_square * theta_square
 
-        # Do not forward close-to-zero denominator gradients
-        omega_singularity = s.abs().squeeze() < self.epsilon
         omega = torch.zeros_like(hv)
-        if (~omega_singularity).sum() > 0:
-            omega[~omega_singularity, :] = theta[~omega_singularity, :] * (
-                hv[~omega_singularity, :] / s[~omega_singularity, :]
-            )
-        if omega_singularity.sum() > 0:
-            omega[omega_singularity, :] = theta[omega_singularity, :] * (
-                hv[omega_singularity, :] / s[omega_singularity, :].data
-            )
+        omega_singularity = s.abs().squeeze() < self.epsilon
+        omega[~omega_singularity] = theta[~omega_singularity] * (hv[~omega_singularity] / s[~omega_singularity])
+        omega[omega_singularity, :] = (
+            1.0
+            + (theta_square[omega_singularity, :] / 6.0)
+            + ((7.0 / 360.0) * theta_fourth[omega_singularity, :])
+        ) * hv[omega_singularity, :]
 
-        mu_r = (c * theta) / s
+        mu_r = torch.zeros_like(theta)
         mu_r_singularity = s.abs() < self.epsilon
-        if mu_r_singularity[mu_r_singularity == True].shape[0] > 0:
-            mu_r[mu_r_singularity] = (
-                1.0
-                - (theta_square[mu_r_singularity] / 3.0)
-                - (theta_fourth[mu_r_singularity] / 45.0)
-            )
-        mu_d = (1.0 - mu_r) / theta_square
+        mu_r[~mu_r_singularity] = (c[~mu_r_singularity] * theta[~mu_r_singularity]) / s[~mu_r_singularity]
+        mu_r[mu_r_singularity] = (
+            1.0
+            - (theta_square[mu_r_singularity] / 3.0)
+            - (theta_fourth[mu_r_singularity] / 45.0)
+        )
+
+        mu_d = torch.zeros_like(mu_r)
         mu_d_singularity = theta_square < self.epsilon
-        if mu_d_singularity[mu_d_singularity == True].shape[0] > 0:
-            mu_d[mu_d_singularity] = (
-                (1.0 / 3.0)
-                + (theta_square[mu_d_singularity] / 45.0)
-                + ((2.0 * theta_fourth[mu_d_singularity]) / 945.0)
-            )
+        mu_d[~mu_d_singularity] = (1.0 - mu_r[~mu_d_singularity]) / theta_square[~mu_d_singularity]
+        mu_d[mu_d_singularity] = (
+            (1.0 / 3.0)
+            + (theta_square[mu_d_singularity] / 45.0)
+            + ((2.0 * theta_fourth[mu_d_singularity]) / 945.0)
+        )
+
         # TODO: try to implement without `einsum`
         inner = torch.einsum("bi,bi->b", v / 2.0, omega).unsqueeze(1)
         log_v = (
@@ -272,11 +323,24 @@ class ImplicitDualQuaternion(SE3):
 
 
 if __name__ == "__main__":
+
+    urdf_robot = UrdfRobotLibrary.from_urdf_path("./urdf/cr5.urdf")
+    open_chain = urdf_robot.extract_open_chains(se3, 0.1)[-1]
+    se3 = ImplicitDualQuaternion()
+    target_pose = torch.tensor([[ 0.31304278969764709473, -0.00631798803806304932,
+        1.22056627273559570312, -1.25425827503204345703,
+        0.11632148176431655884,  0.80687266588211059570]], device='cuda:0')
+    thetas = torch.tensor([[-3.51662302017211914062,  0.27089971303939819336,
+        3.34850096702575683594, -2.87017297744750976562,
+        0.16795067489147186279,  0.00000000000000000000]], device='cuda:0')
+    current_pose = open_chain.forward_kinematics(thetas)
+    '''
     coords = torch.tensor(
         [
             [1, 0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0, 0],
             [0, 0, 1, 0, 0, 0],
+            [0, 0, 0, torch.pi / 2.0, 0, 0],
             [0, 0, 0, torch.pi / 4.0, 0, 0],
             [0, 0, 0, 0, torch.pi / 4.0, 0],
             [0, 0, 0, 0, 0, torch.pi / 4.0],
@@ -288,11 +352,13 @@ if __name__ == "__main__":
             [0, 0, 0, 1, 2, 0, 0],
             [0, 0, 0, 1, 0, 2, 0],
             [0, 0, 0, 1, 0, 0, 2],
+            [math.sin(math.pi / 2.0), 0, 0, math.cos(math.pi / 2.0), 0, 0, 0],
             [math.sin(math.pi / 4.0), 0, 0, math.cos(math.pi / 4.0), 0, 0, 0],
             [0, math.sin(math.pi / 4.0), 0, math.cos(math.pi / 4.0), 0, 0, 0],
             [0, 0, math.sin(math.pi / 4.0), math.cos(math.pi / 4.0), 0, 0, 0],
             [math.sin(math.pi / 4.0), 0, 0, math.cos(math.pi / 4.0), 2, 0, 0],
-        ]
+        ],
+        requires_grad=True,
     ).float()
 
     expected_exp_squared = torch.tensor(
@@ -300,6 +366,7 @@ if __name__ == "__main__":
             [0, 0, 0, 1, 4, 0, 0],
             [0, 0, 0, 1, 0, 4, 0],
             [0, 0, 0, 1, 0, 0, 4],
+            [math.sin(math.pi), 0, 0, math.cos(math.pi), 0, 0, 0],
             [math.sin(math.pi / 2.0), 0, 0, math.cos(math.pi / 2.0), 0, 0, 0],
             [0, math.sin(math.pi / 2.0), 0, math.cos(math.pi / 2.0), 0, 0, 0],
             [0, 0, math.sin(math.pi / 2.0), math.cos(math.pi / 2.0), 0, 0, 0],
@@ -318,4 +385,19 @@ if __name__ == "__main__":
     identities = se3.chain(exp_squared_inv, exp_squared)
     expected_identities = se3.identity(identities.shape[0])
     assert (expected_identities - identities).abs().mean(1).mean(0).item() < 1e-6
+
+    test = torch.tensor(
+        [[math.sin(math.pi / 2.0), 0, 0, math.cos(math.pi / 2.0), 0, 0, 0]],
+        requires_grad=True,
+    ).float()
+
+    zero_h = test  # expected_exp[0:1, :]
+    se3_log = se3.log(zero_h)
+    loss = se3_log.sum()
+    loss.retain_grad()
+    se3_log.retain_grad()
+    loss.backward()
+    print(se3_log.grad)
+    # breakpoint()
     print("ALL TESTS PASSED")
+    '''

linguamechanica/train.py
@@ -49,7 +49,7 @@ def evaluate_policy(open_chain, agent, training_state, summary):
     required=False,
 )
 def train(checkpoint, urdf, level):
-    torch.autograd.set_detect_anomaly(True)
+    #torch.autograd.set_detect_anomaly(True)
     urdf_robot = UrdfRobotLibrary.from_urdf_path(urdf_path=urdf)
     # TODO: make this generic
     se3 = ImplicitDualQuaternion()

linguamechanica/training_context.py
@@ -14,11 +14,11 @@ class TrainingState:
     episode_batch_size: int = 1024
     max_std_dev = 0.002
     save_freq: int = 1000
-    lr_actor: float = 1e-4
+    lr_actor: float = 1e-6
     lr_actor_geodesic_base: float = 1e-4
     gradient_clip_actor_geodesic_base: float = 1.0
     lr_actor_entropy: float = 1e-4
-    lr_critic: float = 1e-4
+    lr_critic: float = 1e-6
     gamma: float = 0.99
     policy_freq: int = 16
     target_update_freq_cte: int = 16
@@ -102,7 +102,7 @@ class EpisodeState:
         self.initial_reward = initial_reward.detach().cpu()
         self.discounted_gamma = gamma
 
-    def step(self, reward, done, step, summary):
+    def step(self, reward, done, time_step, summary):
         self.done = done.detach().cpu()
         if self.discounted_reward is None:
             self.discounted_reward = reward.detach().cpu()
@@ -124,14 +124,18 @@ class EpisodeState:
               =>  0  : No improvement, final reward is equal to initial reward
               => -1  : 100% improvement, final reward is zero
             """
+            if torch.isnan(reward_times_worse).sum() > 0:
+                print("REWARD TIMES WORSE")
+                print(f"Time Step {time_step}")
+                print(torch.isnan(reward_times_worse).sum())
             summary.add_scalar(
                 f"{self.label} / Reward Times Worse",
                 reward_times_worse,
-                step,
+                time_step,
             )
             summary.add_scalar(
                 f"{self.label} / Acc. Disc. Reward",
                 self.discounted_reward.mean(),
-                step,
+                time_step,
             )
         return everything_is_done

tests/linguamechanica/test_kinematics.py
@@ -9,13 +9,13 @@ from linguamechanica.kinematics import (
 import random
 import torch
 from pytorch3d import transforms
-from linguamechanica.se3 import ProjectiveMatrix
+from linguamechanica.se3 import ProjectiveMatrix, ImplicitDualQuaternion
 
 
 class TestDifferentiableOpenChainMechanism(unittest.TestCase):
     def test_compute_error_pose_cr5(self):
         urdf_robot = UrdfRobotLibrary.dobot_cr5()
-        se3 = ProjectiveMatrix()
+        se3 = ImplicitDualQuaternion()
         open_chains = urdf_robot.extract_open_chains(se3, 0.3)
         open_chain = open_chains[-1]
         for _ in range(1000):
@@ -28,11 +28,7 @@ class TestDifferentiableOpenChainMechanism(unittest.TestCase):
                 )
             coordinates = torch.Tensor(coordinates).unsqueeze(0)
             transformation = open_chain.forward_kinematics(coordinates)
-            pose = transforms.se3_log_map(transformation)
-            # current_transformation = open_chain.forward_kinematics(coordinates)
-            torch.set_printoptions(precision=10)
-            transforms.se3_exp_map(pose)
-            # transforms.se3_exp_map(transforms.se3_log_map(transform))
+            pose = se3.log(transformation)
             error_pose = open_chain.compute_error_pose(coordinates, pose)
             expected_error_pose = torch.zeros(error_pose.shape)
             assert (error_pose - expected_error_pose).abs().sum() <= 1e-3